8aafa4301997cc8313f200d23a692dc8c234efd7
antonimagit <31416232+antonimagit@users.noreply.github.com>
2025-11-12 09:36:44 +0100

    eeee

--- APP/GLOBAL_keys.py
+++ APP/GLOBAL_keys.py
@@ -5,6 +5,7 @@ WATSONX_URL = os.getenv("WATSONX_URL", "https://us-south.ml.cloud.ibm.com")
 WATSONX_API_KEY_TZ = os.getenv("WATSONX_API_KEY_TZ", "fpMu8viNLafvrayEkNyXxKjyYWOz9rFqzFBbg47-6U2i")
 WATSONX_PROJECTID_TZ = os.getenv("WATSONX_PROJECTID_TZ", "05a78408-7b0c-4c6f-ad34-5e267488200c")
 LLM_GEN_URL = os.getenv("LLM_GEN_URL", "https://us-south.ml.cloud.ibm.com/ml/v1/text/generation?version=2023-05-29")
+TRUNCATE_INPUT_TOKENS = os.getenv("TRUNCATE_INPUT_TOKENS", 512)
 
 REDIS_HOST = os.getenv("REDIS_HOST", "redis-app-shared")
 REDIS_PORT = os.getenv("REDIS_PORT", "6379")
